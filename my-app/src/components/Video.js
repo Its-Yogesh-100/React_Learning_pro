4190357e8bf90783618b0e9060d271f5fb94b0de
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2023-11-09 14:32:36 +0530

    LEARN ABOUT THE VARIBLE FORMAT IN REACTJS

--- my-app/src/components/Video.js
+++ my-app/src/components/Video.js
@@ -1,12 +1,14 @@
 function Video() {
 
+    let topic="React JS"
+
     return (
         <>
             <img
                 src="https://i.imgur.com/MK3eW3As.jpg"
                 alt="Katherine Johnson" />
 
-            <div className="" style={{ backgroundColor: 'red' }}>React Js Tutorial</div>
+            <div className="" style={{ backgroundColor: 'red' }}>{topic} Tutorial</div>
 
 
         </>

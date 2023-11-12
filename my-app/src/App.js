695c6041193b73956095ba8a81c49d369e2e8593
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2023-11-12 09:45:21 +0530

    learn about the list rendring

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,25 +5,62 @@ import './App.css'
 
 function App(){
 
-  let obj={
+  // let obj={
 
-    title:"MongoDB tutorial",
+  //   title:"MongoDB tutorial",
+  //   channel:"coder dost",
+  //   views:"999k",
+  //   time:"1 year ago",
+  //   verified:true
+
+  // };
+
+    let Videos=[{
+
+    title:"Node js",
     channel:"coder dost",
-    views:"999k",
+    views:"9k",
     time:"1 year ago",
     verified:true
 
-  };
+  },{
 
-  const c="coder dost";
+    title:"Mongo DB ",
+    channel:"coder dost",
+    views:"999k",
+    time:"1 year ago",
+    verified:false
+
+  },{
+
+    title:"TypeScript",
+    channel:"coder dost",
+    views:"1M",
+    time:"2 year ago",
+    verified:true
+
+  }];
+
+  
+
+  // const c="coder dost";
 
   return(
 
     <div className="App">
       <div>
-      <Video  title="React Js" views="10k" time="1 year ago" channel={c} verified={false} ></Video>    
-      <Video  title="Node js" views="100k" time="2 year ago" channel={c} verified={true}></Video>    
-      <Video {...obj}></Video> 
+      {/* <Video  title="React Js" views="10k" time="1 year ago" channel={c} verified={false} ></Video>     */}
+    
+      {
+        Videos.map(video=>
+          <Video  title={video.title} views={video.views} time={video.time} channel={video.channel} verified={video.verified}></Video>
+
+        )
+      }
+          
+    
+    
+      {/* <Video {...obj}></Video>  */}
     </div>
 
   
